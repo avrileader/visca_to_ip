7473f9df598eab91b47ef9499695cbdf32c3fcfa
avrileader <avrileader@163.com>
2025-07-12 01:58:16 +0000

    fix error

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ pub struct Config {
 }
 pub fn config(path: String) -> Config {
     let file = File::open(path).expect("open file failed");
-    let config: Config = serde_yaml::from_reader(file).expect("read railed");
+    let config: Config = serde_yaml::from_reader(file).expect("read failed");
     print!("listen port: {} \n",config.local);
     print!("remote port: {} \n",config.remote);
     return Config{local: config.local, remote: config.remote};
